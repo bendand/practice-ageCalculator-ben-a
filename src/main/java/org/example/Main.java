2e922f79a4af8456028dfa8c10797478244c9a69
Ben <bendanderson13@gmail.com>
2025-06-03 15:04:46 -0500

    complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,23 @@
 package org.example;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        Scanner input = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("This calculator tells you how many years you have until you turn 100.");
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        System.out.println(Main.yearsToOneHundred(age));
+
+        input.close();
+    }
+
+    public static String yearsToOneHundred(int age) {
+        int yearsDifference = 100 - age;
+        return "You have " + yearsDifference + " years until you turn 100";
     }
 }
